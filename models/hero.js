5a9e33bc557c65c68bfa258c2ae98236d7b75763
Kate Preston <ktweeden@googlemail.com>
2018-04-13 13:45:09 +0100

    Added task sorting

--- models/hero.js
+++ models/hero.js
@@ -1,12 +1,49 @@
-const Hero = function(name, health, faveourite_food) {
+// A hero should be able to view tasks that are marked as completed or incomplete.
+
+const Hero = function(name, health, favourite_food) {
   this.name = name;
   this.health = health;
-  this.faveourite_food = faveourite_food;
+  this.favourite_food = favourite_food;
   this.tasks = [];
 }
 
 Hero.prototype.speak = function () {
   return `Hello, I am ${this.name}`;
+};
+
+Hero.prototype.eat = function (food) {
+  let food_value = food.replenishment_value;
+  if(food.name === this.favourite_food) {
+    food_value *= 1.5;
+  }
+  this.health += food_value;
+};
+
+Hero.prototype.addTask = function (task) {
+  this.tasks.push(task);
+};
+
+Hero.prototype.sortTasks = function (property) {
+  this.tasks.sort((taskA, taskB) => {
+    if(taskA[property] > taskB[property]) {
+      return -1;
+    }
+    else if (taskA[property] < taskB[property]) {
+      return 1;
+    }
+
+    else {
+      return 0;
+    }
+  })
+};
+
+Hero.prototype.completeTasks = function () {
+  return this.tasks.filter(task => task.completed === true);
+};
+
+Hero.prototype.incompleteTasks = function () {
+  return this.tasks.filter(task => task.completed === false);
 };
 
 module.exports = Hero;
--- specs/hero_spec.js
+++ specs/hero_spec.js
@@ -1,11 +1,26 @@
 const assert = require('assert');
 const Hero = require('../models/hero.js');
+const Food = require('../models/food.js');
+const Task = require('../models/task.js');
 
 describe('Hero tests', function() {
   let hero;
+  let cheese;
+  let blueberry_pie;
+  let hardTask;
+  let highRewardTask;
+  let easyTask;
+  let urgentTask;
 
   beforeEach(function() {
     hero = new Hero('Gregor the Grotesque', 150, 'Blueberry pie')
+    cheese = new Food('cheese', 5);
+    blueberry_pie = new Food('Blueberry pie', 2);
+    hardTask = new Task(20, 2, 30);
+    highRewardTask = new Task(4, 1, 100);
+    easyTask = new Task(1, 5, 50);
+    urgentTask = new Task(5, 10, 60);
+
   });
 
   it('Has a name', function() {
@@ -16,15 +31,82 @@ describe('Hero tests', function() {
     assert.strictEqual(hero.health, 150);
   });
 
-  it('Has a faveourite food', function() {
-    assert.strictEqual(hero.faveourite_food, 'Blueberry pie');
+  it('Has a favourite food', function() {
+    assert.strictEqual(hero.favourite_food, 'Blueberry pie');
   });
 
   it('Can speak', function() {
     assert.strictEqual(hero.speak(), 'Hello, I am Gregor the Grotesque');
-  })
+  });
 
   it('Has a list of tasks to complete', function() {
     assert.deepStrictEqual(hero.tasks, []);
+  });
+
+  it('Can replenish health by eating food', function() {
+    hero.eat(cheese);
+    assert.strictEqual(hero.health, 155);
+  });
+
+  it('Can replenish health more by eating  favouritefood', function() {
+    hero.eat(blueberry_pie);
+    assert.strictEqual(hero.health, 153);
+  });
+
+  it('Can add new task', function() {
+    hero.addTask(hardTask);
+    assert.deepStrictEqual(hero.tasks, [hardTask]);
+  })
+
+  it('Can sort tasks by difficulty', function() {
+    hero.addTask(hardTask);
+    hero.addTask(highRewardTask);
+    hero.addTask(easyTask);
+    hero.addTask(urgentTask);
+    hero.sortTasks('difficulty');
+    expected = [hardTask, urgentTask, highRewardTask, easyTask];
+    assert.deepStrictEqual(hero.tasks, expected);
+  })
+
+  it('Can sort tasks by urgency', function() {
+    hero.addTask(hardTask);
+    hero.addTask(highRewardTask);
+    hero.addTask(easyTask);
+    hero.addTask(urgentTask);
+    hero.sortTasks('urgency');
+    expected = [urgentTask, easyTask, hardTask, highRewardTask];
+    assert.deepStrictEqual(hero.tasks, expected);
+  })
+
+  it('Can sort tasks by reward', function() {
+    hero.addTask(hardTask);
+    hero.addTask(highRewardTask);
+    hero.addTask(easyTask);
+    hero.addTask(urgentTask);
+    hero.sortTasks('reward');
+    expected = [highRewardTask, urgentTask, easyTask, hardTask];
+    assert.deepStrictEqual(hero.tasks, expected);
+  })
+
+  it('Can get all completed tasks', function() {
+    hero.addTask(hardTask);
+    hero.addTask(highRewardTask);
+    hero.addTask(easyTask);
+    hero.addTask(urgentTask);
+    hardTask.setComplete();
+    expected = [hardTask];
+    assert.deepStrictEqual(hero.completeTasks(), expected);
+  })
+
+  it('Can get all incomplete tasks', function() {
+    hero.addTask(hardTask);
+    hero.addTask(highRewardTask);
+    hero.addTask(easyTask);
+    hero.addTask(urgentTask);
+    highRewardTask.setComplete();
+    easyTask.setComplete();
+    hardTask.setComplete();
+    expected = [urgentTask];
+    assert.deepStrictEqual(hero.incompleteTasks(), expected);
   })
 })
